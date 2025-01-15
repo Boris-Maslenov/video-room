04e9557fd72f48994f63e3626f3a19f4782f2845
maslenov_ba <maslenov_ba@teleformis.ru>
2025-01-15 23:58:04 +0300

    add classnames

--- client/src/components/room/Room.tsx
+++ client/src/components/room/Room.tsx
@@ -1,28 +1,34 @@
 import { useRef, useEffect, RefObject, FC, memo } from "react";
 import { MediaSlotDataType } from "../../room-client/types";
+import classNames from "classnames";
 
-const ConsumerRenderer: FC<{ stream: MediaStream; isSelf: boolean }> = memo(
-  ({ stream, isSelf }) => {
-    const mediaElRef = useRef() as RefObject<HTMLVideoElement>;
+const ConsumerRenderer: FC<{
+  stream: MediaStream;
+  isSelf: boolean;
+  peerName: string;
+}> = memo(({ stream, isSelf, peerName }) => {
+  const mediaElRef = useRef() as RefObject<HTMLVideoElement>;
 
-    useEffect(() => {
-      if (mediaElRef.current) {
-        mediaElRef.current.srcObject = stream;
-        mediaElRef.current.playsInline = true;
-      }
-    }, [mediaElRef.current]);
+  useEffect(() => {
+    if (mediaElRef.current) {
+      mediaElRef.current.srcObject = stream;
+      mediaElRef.current.playsInline = true;
+    }
+  }, [mediaElRef.current]);
 
-    return (
-      <li className={"media-block"}>
-        <video
-          className={isSelf ? "media-elem media-elem__self" : "media-elem"}
-          autoPlay
-          ref={mediaElRef}
-        ></video>
-      </li>
-    );
-  }
-);
+  return (
+    <li className={"media-block"}>
+      <video
+        className={classNames("media-elem", {"media-elem__self": isSelf})}
+        autoPlay
+        ref={mediaElRef}
+      ></video>
+      <div className={classNames("media-block__peer-name", { "self": isSelf })}>
+        {peerName}
+      </div>
+    </li>
+  );
+});
 
 type RoomProps = {
   room: string;
@@ -38,6 +44,7 @@ const Room: FC<RoomProps> = ({ mediaSlots }) => {
           key={key + data.peerId}
           stream={data.mediaStream}
           isSelf={data.isSelf}
+          peerName={data.peerName}
         />
       ))}
     </ul>
