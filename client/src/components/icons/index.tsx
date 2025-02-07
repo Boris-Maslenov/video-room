6409301ed6715a9bc2f29b40030ec0ccc3f23420
maslenov_ba <maslenov_ba@teleformis.ru>
2025-02-07 22:35:05 +0300

    add icons

---
+++ client/src/components/icons/index.tsx
@@ -0,0 +1,7 @@
+export { CloseIcon } from "./CloseIcon";
+export { CameraOnIcon } from "./CameraOnIcon";
+export { CameraOffIcon } from "./CameraOffIcon";
+export { MicOnIcon } from "./MicOnIcon";
+export { MicOffIcon } from "./MicOffIcon";
+export { PhoneIcon } from "./PhoneIcon";
+export { DisplayIcon } from "./DisplayIcon";
